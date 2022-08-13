a62380b98a10173347500f5aff107a21281452df
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-08-13 10:24:34 -0700

    variables in html

--- recipe_manager/views.py
+++ recipe_manager/views.py
@@ -1,10 +1,15 @@
 # render html web pages
-
 from django.http import HttpResponse
-
-htmlpage = """
-<h1>Hello World</h1>
-"""
+import random
 
 def home_view(request):
+    name = 'tomasito'
+    num = random.randint(0,1000)
+    h1_str = f"""
+    <h1>Hello {name}!</h1>
+    """
+    p_str = f"""
+    <p>{num} visits today</p>
+    """
+    htmlpage = h1_str + p_str
     return HttpResponse(htmlpage)
