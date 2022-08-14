0e8464656208165542f7084ad574d2c84f8378de
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-08-14 04:52:16 -0700

    authentication form for login

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,30 @@
 from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(request, username=username, password=password)
-        if not user:
-            context = {
-                'error': 'invalid username or password'
-            }
-            return render(request, 'accounts/login.html', context)
-        print(user)
-        login(request, user)
-        return redirect('/')
-    return render(request, 'accounts/login.html', {})
+        form = AuthenticationForm(request, data=request.POST) # authentication form always takes in request bc it's not creating an instance of a model
+        # username = request.POST.get("username")
+        # password = request.POST.get("password")
+        # user = authenticate(request, username=username, password=password)
+        # if not user:
+        #     context = {
+        #         'error': 'invalid username or password'
+        #     }
+        #     return render(request, 'accounts/login.html', context)
+        # print(user)
+        # login(request, user)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'accounts/login.html', context)
 
 def logout_view(request):
     if request.method == "POST":
