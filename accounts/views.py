31cba286452251c3cd95d1535325915c3cde7443
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-08-13 20:30:12 -0700

    login

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            context = {
+                'error': 'invalid username or password'
+            }
+            return render(request, 'accounts/login.html', context)
+        print(user)
+        login(request, user)
+        return redirect('/')
+    return render(request, 'accounts/login.html', {})
+
+def logout_view(request):
+    return render(request, 'accounts/login.html', {})
+
+def register_view(request):
+    return render(request, 'accounts/login.html', {})
--- recipe_manager/urls.py
+++ recipe_manager/urls.py
@@ -16,13 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from .views import home_view
-from articles import views
+from articles.views import (
+    article_create_view,
+    article_detail_view,
+    article_search_view
+)
+from accounts.views import (
+    login_view
+)
 
 urlpatterns = [
     path('', home_view), #index
-    path('articles/', views.article_search_view),
-    path('articles/create/', views.article_create_view),
-    path('articles/<int:id>/', views.article_detail_view),
+    path('articles/', article_search_view),
+    path('articles/create/', article_create_view),
+    path('articles/<int:id>/', article_detail_view),
     # re_path(r'articles/(?P<id>\d+)/$', home_view) // older versions
     path('admin/', admin.site.urls),
+    path('login/', login_view)
 ]
