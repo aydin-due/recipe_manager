f28e571a20b22c4cb9888577595761cb1a2f6383
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-08-14 10:43:41 -0700

    .env config

--- manage.py
+++ manage.py
@@ -2,10 +2,18 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import dotenv
+import pathlib
 
 def main():
     """Run administrative tasks."""
+    # to read .env regardless of its location, also doesn't run if it doesn't exists
+    # DOT_ENV_PATH = pathlib.Path() / '.env'
+    # if DOT_ENV_PATH.exists():
+    #     dotenv.read_dotenv(str(DOT_ENV_PATH))
+    
+    # to read .env in root and assuming it exists
+    dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_manager.settings')
     try:
         from django.core.management import execute_from_command_line
--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xl(yk7*93)9l5vhxat744hwy)kc4vaizig(pz1wbdnw7q7m)#l'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-xl(yk7*93)9l5vhxat744hwy)kc4vaizig(pz1wbdnw7q7m)#l')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == '1' # 1 == true
 
 ALLOWED_HOSTS = []
-
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
