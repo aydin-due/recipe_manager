76ddb463871525b2bded962b528182d5c8a92137
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-09-01 02:02:35 -0700

    changes to models - datetimefield & datefield

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     content = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # publish = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
+    publish = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
