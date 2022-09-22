298d74dd170b4453e6345853df5026ac9b0f6822
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-09-22 10:53:14 -0700

    recipes app

---
+++ recipes/models.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=228)
+    description = models.TextField(blank=True, null=True)
+    directions = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=228)
+    description = models.TextField(blank=True, null=True)
+    quantity = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
+    directions = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+# class RecipeImage(models.Model):
+#     recipe = models.ForeignKey(Recipe)
